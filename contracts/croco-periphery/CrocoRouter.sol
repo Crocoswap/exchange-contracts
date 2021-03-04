d7861c94550df741539e0df0d768ab803c310123
Óscar Martínez <oscar.martinez.jimenez@alumnos.upm.es>
2021-03-04 18:12:34 +0100

    first commit

---
+++ contracts/croco-periphery/CrocoRouter.sol
@@ -0,0 +1,446 @@
+pragma solidity =0.6.6;
+
+import '../croco-core/interfaces/ICrocoFactory.sol';
+import '../croco-lib/libraries/TransferHelper.sol';
+
+import './interfaces/ICrocoRouter.sol';
+import './libraries/CrocoLibrary.sol';
+import './libraries/SafeMath.sol';
+import './interfaces/IERC20.sol';
+import './interfaces/IWFTM.sol';
+
+contract CrocoRouter is ICrocoRouter {
+    using SafeMath for uint;
+
+    address public immutable override factory;
+    address public immutable override WFTM;
+
+    modifier ensure(uint deadline) {
+        require(deadline >= block.timestamp, 'CrocoRouter: EXPIRED');
+        _;
+    }
+
+    constructor(address _factory, address _WFTM) public {
+        factory = _factory;
+        WFTM = _WFTM;
+    }
+
+    receive() external payable {
+        assert(msg.sender == WFTM); // only accept FTM via fallback from the WFTM contract
+    }
+
+    // **** ADD LIQUIDITY ****
+    function _addLiquidity(
+        address tokenA,
+        address tokenB,
+        uint amountADesired,
+        uint amountBDesired,
+        uint amountAMin,
+        uint amountBMin
+    ) internal virtual returns (uint amountA, uint amountB) {
+        // create the pair if it doesn't exist yet
+        if (ICrocoFactory(factory).getPair(tokenA, tokenB) == address(0)) {
+            ICrocoFactory(factory).createPair(tokenA, tokenB);
+        }
+        (uint reserveA, uint reserveB) = CrocoLibrary.getReserves(factory, tokenA, tokenB);
+        if (reserveA == 0 && reserveB == 0) {
+            (amountA, amountB) = (amountADesired, amountBDesired);
+        } else {
+            uint amountBOptimal = CrocoLibrary.quote(amountADesired, reserveA, reserveB);
+            if (amountBOptimal <= amountBDesired) {
+                require(amountBOptimal >= amountBMin, 'CrocoRouter: INSUFFICIENT_B_AMOUNT');
+                (amountA, amountB) = (amountADesired, amountBOptimal);
+            } else {
+                uint amountAOptimal = CrocoLibrary.quote(amountBDesired, reserveB, reserveA);
+                assert(amountAOptimal <= amountADesired);
+                require(amountAOptimal >= amountAMin, 'CrocoRouter: INSUFFICIENT_A_AMOUNT');
+                (amountA, amountB) = (amountAOptimal, amountBDesired);
+            }
+        }
+    }
+    function addLiquidity(
+        address tokenA,
+        address tokenB,
+        uint amountADesired,
+        uint amountBDesired,
+        uint amountAMin,
+        uint amountBMin,
+        address to,
+        uint deadline
+    ) external virtual override ensure(deadline) returns (uint amountA, uint amountB, uint liquidity) {
+        (amountA, amountB) = _addLiquidity(tokenA, tokenB, amountADesired, amountBDesired, amountAMin, amountBMin);
+        address pair = CrocoLibrary.pairFor(factory, tokenA, tokenB);
+        TransferHelper.safeTransferFrom(tokenA, msg.sender, pair, amountA);
+        TransferHelper.safeTransferFrom(tokenB, msg.sender, pair, amountB);
+        liquidity = ICrocoPair(pair).mint(to);
+    }
+    function addLiquidityFTM(
+        address token,
+        uint amountTokenDesired,
+        uint amountTokenMin,
+        uint amountFTMMin,
+        address to,
+        uint deadline
+    ) external virtual override payable ensure(deadline) returns (uint amountToken, uint amountFTM, uint liquidity) {
+        (amountToken, amountFTM) = _addLiquidity(
+            token,
+            WFTM,
+            amountTokenDesired,
+            msg.value,
+            amountTokenMin,
+            amountFTMMin
+        );
+        address pair = CrocoLibrary.pairFor(factory, token, WFTM);
+        TransferHelper.safeTransferFrom(token, msg.sender, pair, amountToken);
+        IWFTM(WFTM).deposit{value: amountFTM}();
+        assert(IWFTM(WFTM).transfer(pair, amountFTM));
+        liquidity = ICrocoPair(pair).mint(to);
+        // refund dust FTM, if any
+        if (msg.value > amountFTM) TransferHelper.safeTransferFTM(msg.sender, msg.value - amountFTM);
+    }
+
+    // **** REMOVE LIQUIDITY ****
+    function removeLiquidity(
+        address tokenA,
+        address tokenB,
+        uint liquidity,
+        uint amountAMin,
+        uint amountBMin,
+        address to,
+        uint deadline
+    ) public virtual override ensure(deadline) returns (uint amountA, uint amountB) {
+        address pair = CrocoLibrary.pairFor(factory, tokenA, tokenB);
+        ICrocoPair(pair).transferFrom(msg.sender, pair, liquidity); // send liquidity to pair
+        (uint amount0, uint amount1) = ICrocoPair(pair).burn(to);
+        (address token0,) = CrocoLibrary.sortTokens(tokenA, tokenB);
+        (amountA, amountB) = tokenA == token0 ? (amount0, amount1) : (amount1, amount0);
+        require(amountA >= amountAMin, 'CrocoRouter: INSUFFICIENT_A_AMOUNT');
+        require(amountB >= amountBMin, 'CrocoRouter: INSUFFICIENT_B_AMOUNT');
+    }
+    function removeLiquidityFTM(
+        address token,
+        uint liquidity,
+        uint amountTokenMin,
+        uint amountFTMMin,
+        address to,
+        uint deadline
+    ) public virtual override ensure(deadline) returns (uint amountToken, uint amountFTM) {
+        (amountToken, amountFTM) = removeLiquidity(
+            token,
+            WFTM,
+            liquidity,
+            amountTokenMin,
+            amountFTMMin,
+            address(this),
+            deadline
+        );
+        TransferHelper.safeTransfer(token, to, amountToken);
+        IWFTM(WFTM).withdraw(amountFTM);
+        TransferHelper.safeTransferFTM(to, amountFTM);
+    }
+    function removeLiquidityWithPermit(
+        address tokenA,
+        address tokenB,
+        uint liquidity,
+        uint amountAMin,
+        uint amountBMin,
+        address to,
+        uint deadline,
+        bool approveMax, uint8 v, bytes32 r, bytes32 s
+    ) external virtual override returns (uint amountA, uint amountB) {
+        address pair = CrocoLibrary.pairFor(factory, tokenA, tokenB);
+        uint value = approveMax ? uint(-1) : liquidity;
+        ICrocoPair(pair).permit(msg.sender, address(this), value, deadline, v, r, s);
+        (amountA, amountB) = removeLiquidity(tokenA, tokenB, liquidity, amountAMin, amountBMin, to, deadline);
+    }
+    function removeLiquidityFTMWithPermit(
+        address token,
+        uint liquidity,
+        uint amountTokenMin,
+        uint amountFTMMin,
+        address to,
+        uint deadline,
+        bool approveMax, uint8 v, bytes32 r, bytes32 s
+    ) external virtual override returns (uint amountToken, uint amountFTM) {
+        address pair = CrocoLibrary.pairFor(factory, token, WFTM);
+        uint value = approveMax ? uint(-1) : liquidity;
+        ICrocoPair(pair).permit(msg.sender, address(this), value, deadline, v, r, s);
+        (amountToken, amountFTM) = removeLiquidityFTM(token, liquidity, amountTokenMin, amountFTMMin, to, deadline);
+    }
+
+    // **** REMOVE LIQUIDITY (supporting fee-on-transfer tokens) ****
+    function removeLiquidityFTMSupportingFeeOnTransferTokens(
+        address token,
+        uint liquidity,
+        uint amountTokenMin,
+        uint amountFTMMin,
+        address to,
+        uint deadline
+    ) public virtual override ensure(deadline) returns (uint amountFTM) {
+        (, amountFTM) = removeLiquidity(
+            token,
+            WFTM,
+            liquidity,
+            amountTokenMin,
+            amountFTMMin,
+            address(this),
+            deadline
+        );
+        TransferHelper.safeTransfer(token, to, IERC20(token).balanceOf(address(this)));
+        IWFTM(WFTM).withdraw(amountFTM);
+        TransferHelper.safeTransferFTM(to, amountFTM);
+    }
+    function removeLiquidityFTMWithPermitSupportingFeeOnTransferTokens(
+        address token,
+        uint liquidity,
+        uint amountTokenMin,
+        uint amountFTMMin,
+        address to,
+        uint deadline,
+        bool approveMax, uint8 v, bytes32 r, bytes32 s
+    ) external virtual override returns (uint amountFTM) {
+        address pair = CrocoLibrary.pairFor(factory, token, WFTM);
+        uint value = approveMax ? uint(-1) : liquidity;
+        ICrocoPair(pair).permit(msg.sender, address(this), value, deadline, v, r, s);
+        amountFTM = removeLiquidityFTMSupportingFeeOnTransferTokens(
+            token, liquidity, amountTokenMin, amountFTMMin, to, deadline
+        );
+    }
+
+    // **** SWAP ****
+    // requires the initial amount to have already been sent to the first pair
+    function _swap(uint[] memory amounts, address[] memory path, address _to) internal virtual {
+        for (uint i; i < path.length - 1; i++) {
+            (address input, address output) = (path[i], path[i + 1]);
+            (address token0,) = CrocoLibrary.sortTokens(input, output);
+            uint amountOut = amounts[i + 1];
+            (uint amount0Out, uint amount1Out) = input == token0 ? (uint(0), amountOut) : (amountOut, uint(0));
+            address to = i < path.length - 2 ? CrocoLibrary.pairFor(factory, output, path[i + 2]) : _to;
+            ICrocoPair(CrocoLibrary.pairFor(factory, input, output)).swap(
+                amount0Out, amount1Out, to, new bytes(0)
+            );
+        }
+    }
+    function swapExactTokensForTokens(
+        uint amountIn,
+        uint amountOutMin,
+        address[] calldata path,
+        address to,
+        uint deadline
+    ) external virtual override ensure(deadline) returns (uint[] memory amounts) {
+        amounts = CrocoLibrary.getAmountsOut(factory, amountIn, path);
+        require(amounts[amounts.length - 1] >= amountOutMin, 'CrocoRouter: INSUFFICIENT_OUTPUT_AMOUNT');
+        TransferHelper.safeTransferFrom(
+            path[0], msg.sender, CrocoLibrary.pairFor(factory, path[0], path[1]), amounts[0]
+        );
+        _swap(amounts, path, to);
+    }
+    function swapTokensForExactTokens(
+        uint amountOut,
+        uint amountInMax,
+        address[] calldata path,
+        address to,
+        uint deadline
+    ) external virtual override ensure(deadline) returns (uint[] memory amounts) {
+        amounts = CrocoLibrary.getAmountsIn(factory, amountOut, path);
+        require(amounts[0] <= amountInMax, 'CrocoRouter: EXCESSIVE_INPUT_AMOUNT');
+        TransferHelper.safeTransferFrom(
+            path[0], msg.sender, CrocoLibrary.pairFor(factory, path[0], path[1]), amounts[0]
+        );
+        _swap(amounts, path, to);
+    }
+    function swapExactFTMForTokens(uint amountOutMin, address[] calldata path, address to, uint deadline)
+        external
+        virtual
+        override
+        payable
+        ensure(deadline)
+        returns (uint[] memory amounts)
+    {
+        require(path[0] == WFTM, 'CrocoRouter: INVALID_PATH');
+        amounts = CrocoLibrary.getAmountsOut(factory, msg.value, path);
+        require(amounts[amounts.length - 1] >= amountOutMin, 'CrocoRouter: INSUFFICIENT_OUTPUT_AMOUNT');
+        IWFTM(WFTM).deposit{value: amounts[0]}();
+        assert(IWFTM(WFTM).transfer(CrocoLibrary.pairFor(factory, path[0], path[1]), amounts[0]));
+        _swap(amounts, path, to);
+    }
+    function swapTokensForExactFTM(uint amountOut, uint amountInMax, address[] calldata path, address to, uint deadline)
+        external
+        virtual
+        override
+        ensure(deadline)
+        returns (uint[] memory amounts)
+    {
+        require(path[path.length - 1] == WFTM, 'CrocoRouter: INVALID_PATH');
+        amounts = CrocoLibrary.getAmountsIn(factory, amountOut, path);
+        require(amounts[0] <= amountInMax, 'CrocoRouter: EXCESSIVE_INPUT_AMOUNT');
+        TransferHelper.safeTransferFrom(
+            path[0], msg.sender, CrocoLibrary.pairFor(factory, path[0], path[1]), amounts[0]
+        );
+        _swap(amounts, path, address(this));
+        IWFTM(WFTM).withdraw(amounts[amounts.length - 1]);
+        TransferHelper.safeTransferFTM(to, amounts[amounts.length - 1]);
+    }
+    function swapExactTokensForFTM(uint amountIn, uint amountOutMin, address[] calldata path, address to, uint deadline)
+        external
+        virtual
+        override
+        ensure(deadline)
+        returns (uint[] memory amounts)
+    {
+        require(path[path.length - 1] == WFTM, 'CrocoRouter: INVALID_PATH');
+        amounts = CrocoLibrary.getAmountsOut(factory, amountIn, path);
+        require(amounts[amounts.length - 1] >= amountOutMin, 'CrocoRouter: INSUFFICIENT_OUTPUT_AMOUNT');
+        TransferHelper.safeTransferFrom(
+            path[0], msg.sender, CrocoLibrary.pairFor(factory, path[0], path[1]), amounts[0]
+        );
+        _swap(amounts, path, address(this));
+        IWFTM(WFTM).withdraw(amounts[amounts.length - 1]);
+        TransferHelper.safeTransferFTM(to, amounts[amounts.length - 1]);
+    }
+    function swapFTMForExactTokens(uint amountOut, address[] calldata path, address to, uint deadline)
+        external
+        virtual
+        override
+        payable
+        ensure(deadline)
+        returns (uint[] memory amounts)
+    {
+        require(path[0] == WFTM, 'CrocoRouter: INVALID_PATH');
+        amounts = CrocoLibrary.getAmountsIn(factory, amountOut, path);
+        require(amounts[0] <= msg.value, 'CrocoRouter: EXCESSIVE_INPUT_AMOUNT');
+        IWFTM(WFTM).deposit{value: amounts[0]}();
+        assert(IWFTM(WFTM).transfer(CrocoLibrary.pairFor(factory, path[0], path[1]), amounts[0]));
+        _swap(amounts, path, to);
+        // refund dust FTM, if any
+        if (msg.value > amounts[0]) TransferHelper.safeTransferFTM(msg.sender, msg.value - amounts[0]);
+    }
+
+    // **** SWAP (supporting fee-on-transfer tokens) ****
+    // requires the initial amount to have already been sent to the first pair
+    function _swapSupportingFeeOnTransferTokens(address[] memory path, address _to) internal virtual {
+        for (uint i; i < path.length - 1; i++) {
+            (address input, address output) = (path[i], path[i + 1]);
+            (address token0,) = CrocoLibrary.sortTokens(input, output);
+            ICrocoPair pair = ICrocoPair(CrocoLibrary.pairFor(factory, input, output));
+            uint amountInput;
+            uint amountOutput;
+            { // scope to avoid stack too deep errors
+            (uint reserve0, uint reserve1,) = pair.getReserves();
+            (uint reserveInput, uint reserveOutput) = input == token0 ? (reserve0, reserve1) : (reserve1, reserve0);
+            amountInput = IERC20(input).balanceOf(address(pair)).sub(reserveInput);
+            amountOutput = CrocoLibrary.getAmountOut(amountInput, reserveInput, reserveOutput);
+            }
+            (uint amount0Out, uint amount1Out) = input == token0 ? (uint(0), amountOutput) : (amountOutput, uint(0));
+            address to = i < path.length - 2 ? CrocoLibrary.pairFor(factory, output, path[i + 2]) : _to;
+            pair.swap(amount0Out, amount1Out, to, new bytes(0));
+        }
+    }
+    function swapExactTokensForTokensSupportingFeeOnTransferTokens(
+        uint amountIn,
+        uint amountOutMin,
+        address[] calldata path,
+        address to,
+        uint deadline
+    ) external virtual override ensure(deadline) {
+        TransferHelper.safeTransferFrom(
+            path[0], msg.sender, CrocoLibrary.pairFor(factory, path[0], path[1]), amountIn
+        );
+        uint balanceBefore = IERC20(path[path.length - 1]).balanceOf(to);
+        _swapSupportingFeeOnTransferTokens(path, to);
+        require(
+            IERC20(path[path.length - 1]).balanceOf(to).sub(balanceBefore) >= amountOutMin,
+            'CrocoRouter: INSUFFICIENT_OUTPUT_AMOUNT'
+        );
+    }
+    function swapExactFTMForTokensSupportingFeeOnTransferTokens(
+        uint amountOutMin,
+        address[] calldata path,
+        address to,
+        uint deadline
+    )
+        external
+        virtual
+        override
+        payable
+        ensure(deadline)
+    {
+        require(path[0] == WFTM, 'CrocoRouter: INVALID_PATH');
+        uint amountIn = msg.value;
+        IWFTM(WFTM).deposit{value: amountIn}();
+        assert(IWFTM(WFTM).transfer(CrocoLibrary.pairFor(factory, path[0], path[1]), amountIn));
+        uint balanceBefore = IERC20(path[path.length - 1]).balanceOf(to);
+        _swapSupportingFeeOnTransferTokens(path, to);
+        require(
+            IERC20(path[path.length - 1]).balanceOf(to).sub(balanceBefore) >= amountOutMin,
+            'CrocoRouter: INSUFFICIENT_OUTPUT_AMOUNT'
+        );
+    }
+    function swapExactTokensForFTMSupportingFeeOnTransferTokens(
+        uint amountIn,
+        uint amountOutMin,
+        address[] calldata path,
+        address to,
+        uint deadline
+    )
+        external
+        virtual
+        override
+        ensure(deadline)
+    {
+        require(path[path.length - 1] == WFTM, 'CrocoRouter: INVALID_PATH');
+        TransferHelper.safeTransferFrom(
+            path[0], msg.sender, CrocoLibrary.pairFor(factory, path[0], path[1]), amountIn
+        );
+        _swapSupportingFeeOnTransferTokens(path, address(this));
+        uint amountOut = IERC20(WFTM).balanceOf(address(this));
+        require(amountOut >= amountOutMin, 'CrocoRouter: INSUFFICIENT_OUTPUT_AMOUNT');
+        IWFTM(WFTM).withdraw(amountOut);
+        TransferHelper.safeTransferFTM(to, amountOut);
+    }
+
+    // **** LIBRARY FUNCTIONS ****
+    function quote(uint amountA, uint reserveA, uint reserveB) public pure virtual override returns (uint amountB) {
+        return CrocoLibrary.quote(amountA, reserveA, reserveB);
+    }
+
+    function getAmountOut(uint amountIn, uint reserveIn, uint reserveOut)
+        public
+        pure
+        virtual
+        override
+        returns (uint amountOut)
+    {
+        return CrocoLibrary.getAmountOut(amountIn, reserveIn, reserveOut);
+    }
+
+    function getAmountIn(uint amountOut, uint reserveIn, uint reserveOut)
+        public
+        pure
+        virtual
+        override
+        returns (uint amountIn)
+    {
+        return CrocoLibrary.getAmountIn(amountOut, reserveIn, reserveOut);
+    }
+
+    function getAmountsOut(uint amountIn, address[] memory path)
+        public
+        view
+        virtual
+        override
+        returns (uint[] memory amounts)
+    {
+        return CrocoLibrary.getAmountsOut(factory, amountIn, path);
+    }
+
+    function getAmountsIn(uint amountOut, address[] memory path)
+        public
+        view
+        virtual
+        override
+        returns (uint[] memory amounts)
+    {
+        return CrocoLibrary.getAmountsIn(factory, amountOut, path);
+    }
+}
